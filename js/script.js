a1097d437c91a6b79f2d89e9c459771c42994cf1
albertostizzoli <albertostizzoli@yahoo.it>
2023-10-12 14:46:15 +0200

    Complete Array on Mail

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// MAIL
+
+//Array
+const mail = [
+    'pincopallino@example.com',
+    'topolino89@example.com',
+    'pippo95@example.com',
+    'paperino90@example.com',
+    'alberto93@example.com',
+    'alberto80@example.com',
+];
+
