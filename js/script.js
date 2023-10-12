b27c8de2ffbcd51f680bdf872a9fc3e35315bc17
albertostizzoli <albertostizzoli@yahoo.it>
2023-10-12 18:45:23 +0200

    Complete

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ const alert = document.querySelector('.alert');
 const button = document.querySelector('button');
 
 // Creo una funzione che mi permetta di effettuare un check della mail
-button.addEventListener('click', function() {
+button.addEventListener('click', function () {
     alert.className = '.alert .d-none';
     let utentMail = document.getElementById('mail').value;
     let founded = false;
@@ -24,48 +24,50 @@ button.addEventListener('click', function() {
         let utentName = mail[i];
         if (utentName === utentMail) {
             founded = true;
-        } 
+        }
     }
     let text, warning;
-        // Se la mail viene trovata
-        if (founded) {
-            warning = 'alert-success';
-            text = ` Bentornato `;
+    // Se la mail viene trovata
+    if (founded) {
+        warning = 'alert-success';
+        text = ` Bentornato `;
         //Altrimenti
-        } else {
-            warning = 'alert-danger';
-            text = ` L'utente non è registrato `;
-        }
-        alert.innerText = text;
-        alert.className.add('.alert-success');
-        alert.className.remove('.d-none');
+    } else {
+        warning = 'alert-danger';
+        text = ` L'utente non è registrato `;
+    }
+    alert.innerText = text;
+    alert.className.add('.alert-success');
+    alert.className.remove('.d-none');
 }
 );
 
 // ESERCIZIO DADI
 
-//creo una costante che mi generi un numero da 1 a 6 per il giocatore
-const player = document.getElementById('player').value;
-
-// creo una costante che mi generi un numero da 1 a 6 per il computer
-const IA = document.getElementById('cpu').value;
-
-play.addEventListener('click', function(){
-    let player_1 = Math.floor((Math.random()* 6) +1);
-    let playerIA = Math.floor((Math.random()* 6) +1);
+play.addEventListener('click', function () {
+    let player_1 = Math.floor((Math.random() * 6) + 1);
+    let playerIA = Math.floor((Math.random() * 6) + 1);
     //Condizioni che eseguirannno il risultato finale
     let result = document.getElementById('play');
-    if(player_1 > playerIA){
-       Risultato = `Giocatore 1 hai vinto`;
-    } else if(player_1 < playerIA){
-      Risultato = `Giocatore 1 hai perso`;
+    if (player_1 > playerIA) {
+        Risultato = `Giocatore 1 hai vinto`;
+    } else if (player_1 < playerIA) {
+        Risultato = `Giocatore 1 hai perso`;
     } else {
-       Risultato = `Pareggio `;
+        Risultato = `Pareggio `;
     }
-    result.innerHTML= Risultato;
+    result.innerHTML = Risultato;
+    const player = document.getElementById('player');
+    player.innerHTML = `<strong>Giocatore 1</strong> ${randomInteger(1, 6)}`;
+
+    const cpu = document.getElementById('cpu');
+    cpu.innerHTML = `<strong>CPU'</strong> ${randomInteger(1, 6)}`;
 }
 );
-function randomInteger(min,max){
-    return Math.floor (Math.random() * (max -min + 1)) + min;
+function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
+
 
