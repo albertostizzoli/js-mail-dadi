3b75ec44b7b93e04bc9824db8c129386570dea59
albertostizzoli <albertostizzoli@yahoo.it>
2023-10-12 15:28:48 +0200

    Complete Mail

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@
 //Array
 const mail = [
     'pincopallino@example.com',
-    'topolino89@example.com',
-    'pippo95@example.com',
+    'topolino@example.com',
+    'pippo@example.com',
     'paperino90@example.com',
     'alberto93@example.com',
     'alberto80@example.com',
@@ -12,16 +12,30 @@ const mail = [
 
 // Variabili
 const alert = document.querySelector('.alert');
-const button = document.querySelector('.btn-primary');
+const button = document.querySelector('button');
 
 // Eventi
-button.addEventListener('click', function(){
-    alert.className = 'alert d-none';
+button.addEventListener('click', function () {
+    alert.className = '.alert .d-none';
     let utentMail = document.getElementById('mail').value;
     let founded = false;
     //Condizioni
-    
-
+    for (let i = 0; i < mail.length; i++) {
+        let utentName = mail[i];
+        if (utentName === utentMail) {
+            founded = true;
+        } let text, warning;
+        if (founded) {
+            warning = '.alert-success';
+            text = ` Bentornato `;
+        } else {
+            warning = '.alert-danger';
+            text = ` L'utente non è registrato `;
+        }
+        alert.innerText = text;
+        alert.className.add(warning);
+        alert.className.remove('.d-none');
+    }
 }
 );
 
